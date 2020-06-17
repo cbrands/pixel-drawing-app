229fab2332e725a44703fcee3fd01bbd45574c26
Charles Brands <charles.brands@gmail.com>
2020-06-17 09:35:19 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Canvas from "./components/Canvas";
+import ColorPicker from "./components/ColorPicker";
 
 export default function App() {
+  const [color, setColor] = useState(0);
   return (
     <div className="App">
-      <Canvas />
+      <ColorPicker currentColor={color} setColor={color => setColor(color)} />
+      <Canvas currentColor={color} />
     </div>
   );
 }
